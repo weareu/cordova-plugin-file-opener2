d36b1a7fb86fe324f6093da1b4483e9a4a2d3f66
Fanie Oosthuysen <fanie.oosthuysen@gmail.com>
2022-07-21 11:18:53 +0200

    Update Cordova Electron to get correct Args.

--- src/electron/FileOpener2.js
+++ src/electron/FileOpener2.js
@@ -22,13 +22,25 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 // https://www.electronjs.org/docs/api/shell
 const { shell } = global.require('electron');
-module.exports = {
-  open: function (onSuccess, onError, fileName) {
-    var opn = shell.openItem(fileName[0]);
-    if (opn === true) {
-      onSuccess(true);
-    } else {
-      onError({'status': 0, 'message': 'Failed opening file.'});
-    }
+
+//Fix Cordova Electron 3.0 Args Bug. Electron context bridge uses ... for receiving args. This causes
+//args to be nested in another array when sent, this is not like other platform args and makes plugin compatibility hard. 
+// This may change in future so we'll handle it here.
+function getArgs(args) {
+  if(Array.isArray(args) && args.length === 1 && Array.isArray(args[0])) {
+      return args[0];
+  }
+  else {
+      return args;
+  }
+}
+
+exports.open = function (args) {
+  args = getArgs(args);
+  var opn = shell.openItem(args[0]);
+  if (opn === true) {
+    return Promise.resolve(true);
+  } else {
+    return Promise.reject({'status': 0, 'message': 'Failed opening file.'});
   }
 };
